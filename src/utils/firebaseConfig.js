6b528fc8d1380d82df757397e7579887d899d4c4
matheusz-nied <matheustae@hotmail.com>
2024-08-30 18:14:51 -0300

    fix: next_public

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -1,17 +1,21 @@
+// Em uma rota de API do Next.js (por exemplo, pages/api/secureFirebaseCall.js)
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.FIREBASE_API_KEY, // Sem o prefixo NEXT_PUBLIC_
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db };
+export default async function handler(req, res) {
+  // Lide com suas operações seguras aqui
+  res.status(200).json({ message: "Operação segura bem-sucedida" });
+}
